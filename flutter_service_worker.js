fcd7cfb7c02721b7ace37859fc775c30b1ca34ce
clkhoo5211 <clkhoo5211@gmail.com>
2026-02-24 09:13:30 +0800

    v3 - 20260224

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8afd751d4e3f8904c083fc6712840d95",
+const RESOURCES = {"flutter_bootstrap.js": "5b355beda7de8ff856e9ba5ad6b7ae45",
 "version.json": "a85781f18f5d00475c0a7ba72d9d3632",
-"index.html": "80597c9795d43b354447a5b141d6a2ba",
-"/": "80597c9795d43b354447a5b141d6a2ba",
+"index.html": "a1b4a07eebbe3c660f457dcd8625d4bb",
+"/": "a1b4a07eebbe3c660f457dcd8625d4bb",
 "main.dart.js": "06d23f904008418bc06c6cb0120cb10f",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "2cd8bde463f5d82aae0f0cec061d6b8f",
